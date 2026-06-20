1aae530c5908bd9b26425f89d2b05cfbc8618770
ahmedmubasher <mbashrahmd7@gmail.com>
2026-06-20 23:53:26 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -112,9 +112,6 @@ def predict():
 # =========================
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True,
-        use_reloader=False
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
     )
